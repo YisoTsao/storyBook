50da15b298997fb416041520badb94eaf03646c0
garytsao525 <garytsao@kingsinfo.com.tw>
2024-03-15 14:41:04 +0800

    remove ckeditor5

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,9 @@
 import React, { useState } from 'react';
-import dynamic from 'next/dynamic';
-
 import { Step, BaseProgress } from '../components';
 import CustomUpload from '../components/UploadImage/CustomUpload';
 import DropImage from '../components/DropImage';
 
-const CkEditor = dynamic(() => import('../components/Ckeditor'), {
-  ssr: false,
-});
-
 const Home = () => {
-  const [content, setContent] = useState('');
   const [file, setFile] = useState(null);
   const [, setFiles] = useState(null);
 
@@ -24,7 +17,6 @@ const Home = () => {
       <CustomUpload {...iconProps} />
       <Step className="p-8" stepList={fakeStep} />
       <BaseProgress type="step" value="28" max="100" size="large" />
-      <CkEditor content={content} setContent={setContent} />
     </div>
   );
 };
